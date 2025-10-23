9fc265eaa463eb33b82442803eb8c9403c57d15c
nuwangi2003 <nuwangikaushalya18gmail.com>
2025-10-23 22:13:59 +0530

    data insation folder added

---
+++ conrtribution/data insation tg-1736/Result.sql
@@ -0,0 +1,17 @@
+INSERT INTO result (student_id, academic_year, semester, sgpa, cgpa, total_credits) VALUES
+
+('U013',1,'2',NULL,NULL,0),
+('U014',1,'2',NULL,NULL,0),
+('U015',1,'2',NULL,NULL,0),
+('U016',1,'2',NULL,NULL,0),
+('U017',1,'2',NULL,NULL,0),
+('U018',1,'2',NULL,NULL,0),
+('U019',1,'2',NULL,NULL,0),
+('U020',1,'2',NULL,NULL,0),
+('U021',1,'2',NULL,NULL,0),
+('U022',1,'2',NULL,NULL,0),
+('U023',1,'2',NULL,NULL,0),
+('U024',1,'2',NULL,NULL,0),
+('U025',1,'2',NULL,NULL,0),
+('U026',1,'2',NULL,NULL,0),
+('U027',1,'2',NULL,NULL,0);
